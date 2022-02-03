af627ce81f2130a0ea06ade2fe0963b8917417ee
patelr124 <70537858+patelr124@users.noreply.github.com>
2022-02-03 12:13:48 -0500

    added lab2

---
+++ Lab2/lab02_ex3.c
@@ -0,0 +1,21 @@
+/** Riya Patel
+CSC 345-01
+Lab 2 Exercise 1
+**/
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+
+int main (int argc, char** argv){
+    pid_t pid = fork();
+
+    if(pid == 0){
+
+    } else if (pid >0){
+        sleep(30);
+    }
+
+    return 0;
+}
