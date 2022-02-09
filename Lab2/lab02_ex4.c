6d941a252502f04bf5e41bcb78089dc0450f971d
patelr124 <70537858+patelr124@users.noreply.github.com>
2022-02-09 18:13:34 -0500

    ex4 w right output

--- Lab2/lab02_ex4.c
+++ Lab2/lab02_ex4.c
@@ -11,38 +11,39 @@ Lab 2 Exercise 4
 #include <sys/mman.h>
 #include <sys/shm.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
 
 int main(int argc, char** argv){
 
     int n = atoi(argv[1]);
-    pid_t id = fork();
 
-    const char* name = "OS";
+    const char* name = "COLLATZ";
     const int SIZE = 4096;
     int shm_fd;
     void *ptr;
 
-    shm_fd = shm_open(name, SHM_RDONLY, 0666);
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
     ftruncate (shm_fd, SIZE);
-    ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+    ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-
-    if (id ==0){
+    pid_t id = fork();
+    
+    if (id == 0){
         while ( n > 1 ){
-            sprintf(ptr, "%d", n);
-            ptr++;
+            sprintf(ptr, "%d%s", n, " ");
+            ptr += strlen(ptr);
             if( n % 2 == 0){
                 n = n/2;
             } else {
-                n = n*3 + 1;
+                n = 3* n  + 1;
             }
         }
-        sprintf(ptr, "%d", n);
-        ptr++;
-
+        sprintf(ptr, "%d%s", n, " ");
+        ptr += strlen(ptr);
     }  else {
         wait(NULL);
-        printf("%s/n", (char *)ptr);
+        printf("%s\n", (char *)ptr);
         shm_unlink(name);
     }
     return 0;
